2cdf8e5893fc6d0c2f82ede57b027fc8e31cbb22
watarukura <what.r.j150@gmail.com>
2020-07-11 08:12:27 +0900

    test use .env.test

--- tests/e2e/test_mysql.py
+++ tests/e2e/test_mysql.py
@@ -15,16 +15,19 @@ runner = CliRunner()
             '[{"Database": "information_schema"}, {"Database": "test"}]\n',
         ),
         (
-            "CREATE TABLE aaa(bbb varchar(8) primary key);",
+            "CREATE TABLE test_table(test_column varchar(20) primary key);",
             '"affected_rows: 0"\n',
         ),
-        ("SHOW TABLES;", '[{"Tables_in_test": "aaa"}]\n'),
+        ("SHOW TABLES;", '[{"Tables_in_test": "test_table"}]\n'),
         (
-            "SHOW COLUMNS FROM aaa;",
-            '[{"Field": "bbb", "Type": "varchar(8)", "Null": "NO", "Key": "PRI", "Default": null, "Extra": ""}]\n',
+            "SHOW COLUMNS FROM test_table;",
+            '[{"Field": "test_column", "Type": "varchar(20)", "Null": "NO", "Key": "PRI", "Default": null, "Extra": ""}]\n',
         ),
-        ('INSERT INTO aaa(bbb) VALUES("ccc");', '"affected_rows: 1"\n',),
-        ("SELECT * FROM aaa;", '[{"bbb": "ccc"}]\n',),
+        (
+            'INSERT INTO test_table(test_column) VALUES("test_value");',
+            '"affected_rows: 1"\n',
+        ),
+        ("SELECT * FROM test_table;", '[{"test_column": "test_value"}]\n',),
     ),
 )
 def test_cli(sql: str, expect: str):
