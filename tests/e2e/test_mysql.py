3233dad365644e0c5987532abfcbd8d3a09144eb
watarukura <what.r.j150@gmail.com>
2020-07-11 00:10:38 +0900

    add test

--- tests/e2e/test_mysql.py
+++ tests/e2e/test_mysql.py
@@ -15,14 +15,16 @@ runner = CliRunner()
             '[{"Database": "information_schema"}, {"Database": "test"}]\n',
         ),
         (
-            "CREATE TABLE aaa(aaa varchar(8) primary key);",
+            "CREATE TABLE aaa(bbb varchar(8) primary key);",
             '"affected_rows: 0"\n',
         ),
         ("SHOW TABLES;", '[{"Tables_in_test": "aaa"}]\n'),
         (
             "SHOW COLUMNS FROM aaa;",
-            '[{"Field": "aaa", "Type": "varchar(8)", "Null": "NO", "Key": "PRI", "Default": null, "Extra": ""}]\n',
+            '[{"Field": "bbb", "Type": "varchar(8)", "Null": "NO", "Key": "PRI", "Default": null, "Extra": ""}]\n',
         ),
+        ('INSERT INTO aaa(bbb) VALUES("ccc");', '"affected_rows: 1"\n',),
+        ("SELECT * FROM aaa;", '[{"bbb": "ccc"}]\n',),
     ),
 )
 def test_cli(sql: str, expect: str):
